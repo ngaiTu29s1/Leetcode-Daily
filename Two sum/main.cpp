a75a04258f8ac82ef11a45c748682262504cd59c
tudaddy <91655254+tudaddy@users.noreply.github.com>
2025-03-31 15:20:05 +0700

    Initial commit

---
+++ Two sum/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+class Solution {
+    public:
+        std::vector<int> twoSum(std::vector<int>& nums, int target) {
+            std::vector<int> res;
+            std::map<int, int> tracking;
+            for (int i = 0; i < nums.size(); i++){
+                int curr = nums[i];
+                if (tracking.find(target - curr) != tracking.end()){
+                    res.push_back(tracking[target - curr]);
+                    res.push_back(i);
+                    break;
+                }
+                tracking[curr] = i;
+            }
+            return res;
+        }
+    };
+
+int main(){
+    Solution sol;
+    std::vector<int> nums = {2, 7, 11, 15}; // Example input
+    int target = 9; // Example target
+    std::vector<int> solve = sol.twoSum(nums, target);
+    std::cout << "Indices: ";
+    for (int index : solve) {
+        std::cout << index << " ";
+    }
+    return 0;
+}
